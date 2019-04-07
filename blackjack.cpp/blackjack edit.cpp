0fa87f483528d08c385a3057c2fdefde196d8f3e
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-07 16:42:19 +0700

    sun

--- blackjack.cpp/blackjack edit.cpp
+++ blackjack.cpp/blackjack edit.cpp
@@ -6,17 +6,17 @@
 #include<vector>
 #include<string>
 using namespace std;
-class Player;
+class Player
 {
    
-}
-class dealer;
+};
+class dealer
 {
-    void 
-}
-class card;
-{
-    string card[]={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    
+};
+class card
+{   int round=1;
+    string card[]={ "A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 public: 
 void cardscore(string);
 void drawcard(vector<int> &);
@@ -24,5 +24,20 @@ void drawcard(vector<int> &);
 };
 class rule
 {
+
+};
+void card :: drawcard(vector<int> &x)
+{   
+    if(round==1)
+    {
+    for(int i=0;i<2;i++)
+        {
+  x.push_back(rand()%13);
+        }
+    }
+}
+int main()
+{
+
 
 }
