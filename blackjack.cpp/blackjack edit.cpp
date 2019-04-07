b690aa6b9736d416848574822b1efad334ac91ad
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-07 16:32:44 +0700

    sun

---
+++ blackjack.cpp/blackjack edit.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<algorithm>
+#include<ctime>
+#include<vector>
+#include<string>
+using namespace std;
+class Player;
+{
+   
+}
+class dealer;
+{
+    void 
+}
+class card;
+{
+    string card[]={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+public: 
+void cardscore(string);
+void drawcard(vector<int> &);
+
+};
+class rule
+{
+
+}
