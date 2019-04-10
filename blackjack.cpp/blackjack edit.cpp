5a9cb5ede62253373a9d6432159698a8e8681666
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-10 15:05:25 +0700

    hiuhiuk

--- blackjack.cpp/blackjack edit.cpp
+++ blackjack.cpp/blackjack edit.cpp
@@ -7,45 +7,105 @@
 #include<string>
 using namespace std;
 class Player
-{
-   
-};
-class dealer
-{
+    {
+        int score;
+    public:
+        string playername;
+        vector<int> playercard;
+    };
     
-};
-class card
-{   
-    string card[]={ "A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-public: 
-int round=1;
-void cardscore(string);
-void drawcard(vector<int> &);
-
-};
-class rule
-{
-
-};
-void card :: drawcard(vector<int> &x)
-{   
-    int a;
-    if(round==1)
+    
+class dealer
     {
-    for(int i=0;i<2;i++)
+        
+    };
+class allcard
+    {   
+        string card[]={ "s","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        
+           
+    public: 
+    vector<int>scorecard;
+    allcard(vector<int>);
+    int round=1;
+    void cardscore();
+    void drawcard(vector<int> &);
+    void checkcard();
+    vector<int> scorecards(vector<int>);
+    void blackjack();
+    };
+
+vector<int> allcard ::scorecards(vector <int>playercard)
+    {
+        for(int i=0; i<playercard.size(); i++)
+         scorecard.push_back(playercard[i]);
+         return scorecard;
+    }
+        
+        allcard::allcard(vector<int>x)
         {
-  x.push_back(rand()%13);
-        a=x;
+
         }
-        round++;
-    }
-     else
-    {
-        x.push_back(rand()%13);
-    }
-}
+        void allcard :: drawcard(vector<int> &x)
+        {      
+        
+            if(round==1)
+            {
+            for(int i=0;i<2;i++)
+            {
+                x.push_back(rand()%13+1);
+            }
+            round++;
+            }
+        else
+            {
+            x.push_back(rand()%13+1);
+            }
+        }
+        void allcard :: cardscore()
+        {
+        
+        
+            /*if(a[0]==card[1]&&a[1]==card[9]||a[0]==card[1] && a[1]==card[11])
+            {
+                card[1]=11;
+            }
+            else if (a[1]==card[1]&&a[0]==card[9]||a[1]==card[1] && a[0]==card[11])
+            {
+            card[1]=11;
+            }
+            else {card[1]=1;}*/
+            for(int i=2;i<10;i++)
+        {
+            card[i]=i;
+            }
+            for(int j=11;j<14;j++)
+            {
+                card[j]=10;
+            }
+
+        }
+        void allcard ::blackjack()
+            {
+                if(this->playercard[0]+this->playercard[1]==21)
+                    {
+                        cout>>"Blackjack";
+                        
+                    }
+
+    return 0;
+            }
+
+        
 int main()
 {
-
-
+    cout<< scorecard[1];     /*Player p[4];
+   /*for(int i=0;i<2;i++)
+   {
+        for(int j=0;j<4;j++)
+        [
+            p[j].drawcard(p[j].playercard);
+        ]
+   }
+        */
 }
