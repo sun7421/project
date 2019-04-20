e7f76653b76e3f095eef235450ac97ae4f7ede06
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-20 18:26:52 +0700

    kk

--- BJ full/dealer.cpp
+++ BJ full/dealer.cpp
@@ -1,17 +1,89 @@
-#include "card.cpp"
-#include "player.cpp"
+#include<iostream>
+#include<cstdlib>
+#include<algorithm>
+#include<ctime>
+#include<vector>
+using namespace std;
+class card
+{
+int A = 1;
+int J = 10;
+int Q = 10;
+int K = 10;
+public:
+    int round=1;
+    vector<int> cards;
+   
+    void allcard();
+    void shuffler();
+    void showcards();
+   // friend int drawcard();
+    bool cardempty();
+    
+};
+void card::allcard()
+{       
+    cards = {A,2,3,4,5,6,7,8,9,10,J,Q,K,
+             A,2,3,4,5,6,7,8,9,10,J,Q,K,
+             A,2,3,4,5,6,7,8,9,10,J,Q,K, 
+             A,2,3,4,5,6,7,8,9,10,J,Q,K};
+    
+           
+           
+}
+void card::shuffler() {
+	srand(time(0));
+	for (int i = 0; i < 52; i++) 
+    { 
+        swap(cards[i], cards[rand() % 52]);
+	    cout << " Deck Shuffled!******" << endl;
+    }
+}
+void card::showcards() 
+{
+	cout << "card:" << endl;
+	for (int i=0;i<round;i++)
+     {
+		cout << this->cards << " ,";
+	 }
+}
+bool card:: cardempty()
+{
+    return cards.empty();
+}
+
+/*int drawcard() 
+{
+    
+	int c= cards.front();
+	card.erase(cards.begin());
+	return c;
+    
+}*/
+
+int drawcard(vector<int> & cards) 
+{
+    
+	int c= cards.front();
+	cards.erase(cards.begin());
+	return c;
+    
+}
+
 class dealer{
 	vector<int>hand;
 	vector<Player*>playerSet;
+	card draws;
 
 public:
 	void setPlayerSet(vector<Player*>& playerSet);
 	vector<Player*> getPlayerSet();
 	void Draw2();
-	void draw();
 	int CheckHand();
+	void draw();
 
 };
+
 void dealer::setPlayerSet(vector<Player*>& playerSet) {
 	this->playerSet = playerSet;
 }
@@ -20,13 +92,15 @@ vector<Player*> dealer::getPlayerSet() {
 	return this->playerSet;
 }
 
-void dealer::Hit() {
-	this->hand.push_back(drawcard());
+void dealer::draw() {
+	this->hand.push_back(drawcard(draws.cards));
 }
 void dealer::Draw2() {
-	
-	this->draw();
-	this->draw();
+	//int collect;
+	for(int i=0; i<2; i++){
+	//	collect = drawcard(draws.cards);
+		hand.push_back(drawcard(draws.cards));
+	}
 
 	for (auto p : this->playerSet) {
 		p->draw();
