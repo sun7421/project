90b5d03d2f44fc7b523972f6138b806bf5a7b878
winnieinyourarea <thepiggywin2000@gmail.com>
2019-04-26 01:00:44 +0700

    d

--- 24.cpp
+++ 24.cpp
@@ -3,26 +3,26 @@
 #include<cstdlib>
 #include<vector>
 #include<cmath>
+#include<string>
 using namespace std;
 class number
 {
-    public: 
+    public:
     int x,j,k,l,i;
-    int A=rand()%4+1; 
+    int A;
     int ans;
     int answer;
     vector<int>y;
     void numb();
     void shuffle();
     int numbcal();
-    void solution();
     void giveque();
     void Checkwin();
 };
- 
+
 void number::Checkwin()
-{   
-    char t;
+{
+    string t;
         cout<< "Show your knowledge please?\n";
         cout<<"Your answer is? = ";
         cin>>answer;
@@ -30,15 +30,15 @@ void number::Checkwin()
         {
             if(answer=ans)
             {cout<<"You so genius";}
-            else 
-            {   
-                cout<< "ํYou are stupid";
+            else
+            {
+                cout<< "You are stupid";
                for(int j=0;j<1;j++)
                {
                 cout<<"Stupid do you want a solution? (yes or no ): ";
                 cin>> t;
-                if(t='yes'||'Yes'){cout<< "if your brain is a superhero ,You are a invisibleman\n";solution();}
-                else if (t='no'||'No'){i=0;}
+                if(t=="yes"||"Yes"){cout<< "if your brain is a superhero ,You are a invisibleman\n";solution();}
+                else if (t=="no"||"No"){i=0;}
                 else {cout<<"Invalid";j=0;}
                }
 
@@ -50,13 +50,13 @@ void number::giveque()
 {
     switch (A)
     {
-        case 1: 
+        case 1:
         cout <<  " = ((" << y[l] <<" * " << y[i] << ") + " << y[j] <<") - " << y[k]<<"= ";
         break;
         case 2:
         cout << " = ((" << y[i] <<" + " << y[l] << ") * " << y[k] <<") - " << y[j]<<"= ";
         break;
-        case 3: 
+        case 3:
         cout <<  " = ((" << y[i] <<" - " << y[l] << ") * " << y[j] <<") - " << y[k]<<"= ";
         break;
         case 4:
@@ -70,7 +70,7 @@ void number::giveque()
 void number::numb()
 {
     cout<<"I will show you a question\n";
-    f   or(int i=0;i<4;i++)
+    for(int i=0;i<4;i++)
        y.push_back(rand()%99+1);
 }
 void number::shuffle()
@@ -79,8 +79,9 @@ void number::shuffle()
         swap(y[i],y[rand()%4]);
 }
 int number::numbcal()
-{   
-     i = rand()%4;
+{
+    A = rand()%4+1;
+    i = rand()%4;
     ans=0;
     if(i=0){ j= i+1; k=i+2;l=i+3;}
     else if (i=1){ j= i-1; k=i+1;l=i+2;}
@@ -97,7 +98,7 @@ int number::numbcal()
         case 4:ans=((y[j]+y[k])*y[l])-y[i];
             return ans;
     }
-   
+
 }
 
 
