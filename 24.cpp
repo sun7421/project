2253ecc3488b488ef16c45556279ddc243b99b09
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-25 22:50:07 +0700

    sun

--- 24.cpp
+++ 24.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 #include<ctime>
+#include<cstdlib>
+#include<vector>
 #include<cmath>
 using namespace std;
 class number
 {
-    int a;
     public: 
-    number::number(int);
-
+    int x; 
+    void numb();
 };
 int main()
 {
-
-};
+   // srand(time(0));
+    return 0;
+}
