fb5bb2add34fdbde0ce5545d9891e9dbb1f81e8f
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-25 23:31:13 +0700

    sun

--- 24.cpp
+++ 24.cpp
@@ -29,9 +29,12 @@ void number::shuffle()
 }
 void number::numbcal()
 {   
-    int A;
-    for(int i=0;i<3;i++)
-   {    int j= i+1;int k=i+2;int l=i+3;
+    int A,j,k,l;
+    for(int i=0;i<4;i++)
+   {    if(i=0){ j= i+1; k=i+2;l=i+3;}
+        else if (i=1){ j= i-1; k=i+1;l=i+2;}
+        else if (i=2){ j= i-2; k=i-1;l=i+1;}
+        else if (i=3){ j= i-3; k=i-2;l=i+1;}
         switch (A)
             case 1: ((y[l]*y[i])+y[j])-y[k];
             case 2:  ((y[i]+y[l])*y[k])-y[j];
