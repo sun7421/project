47465ae7efce26f84c9736e12456a78dfea65a7b
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-25 23:15:34 +0700

    sun

--- 24.cpp
+++ 24.cpp
@@ -8,16 +8,36 @@ class number
 {
     public: 
     int x; 
+    vector<int>y;
     void numb();
+    void shuffle();
+    void numbcal();
 };
-
+void number::numb()
+{
+ for(int i=0;i<4;i++)
+    {   y.push_back(rand()%99+1);
+        cout<<y<<" ";
+    }
+}
+void number::shuffle()
+{
+	for (int i = 0; i < 4; i++)
+    {
+        swap(y[i],y[rand()%4]);
+    }
+}
+void number::numbcal()
+{
+    for(int i=0;i<4;i++)
+   {    int j= i+1;int b;
+        y[i]+y[j];
+   }
+}
 
 
 int main()
 {
     srand(time(0));
-    vector<int> numb;
-    
-
     return 0;
 }
