51d5e1f56f7f7393225129e02e8cf663fb5f21c3
sunnarinchot <sunnarinchot@sunnarinchots-MacBook-Pro.local>
2019-04-25 21:56:51 +0700

    sun

--- twogirlonecup.cpp
+++ twogirlonecup.cpp
@@ -22,62 +22,40 @@ void player:: bot1()
     z.push_back("paper");
     z.push_back("rock");
     string a  = z[rand()%3];
-    int b =a.length();
-    char char_array[b + 1];
+    char char_array[ a.length()+ 1];
     strcpy(char_array, a.c_str());
-    for (int i = 0; i < b; i++) 
-         char_array[i];
     strcpy(bot,char_array) ;
-    cout<<bot<<"\n";
+    cout<<"bot= "<<bot<<"\n";
 }
 void player:: chub()
 {
-    cout<<"input player1 :";
+    cout<<"choose paper,scissors or rock :";
         scanf("%s" ,player1);
-       
 }
 void player:: check()
 {
         if(!strcmp(bot,player1))
-        {
-            cout<<"Draw\n";
-        }
-        else if(strcmp("paper",player1))
+            {cout<<"Draw\n";}
+         else if(!strcmp("paper",player1))
             {
-                    if(strcmp("rock",bot))
-                    {
-                        cout<<"player1 win";
-                    }
-                    else
-                    {
-                        cout<<"bot win";
-                    }
+                    if(!strcmp("rock",bot))
+                    cout<<"player1 win\n";
+                    else cout<<"bot win\n";
             }
-        else if(strcmp("scissors",player1))
+         else if(!strcmp("scissors",player1))
             {
-                    if(strcmp("paper",bot))
-                    {
-                    cout<<"player1 win";
-                    }
-                    else
-                    {
-                    cout<<"bot win";
-                    }
+                    if(!strcmp("paper",bot))
+                    cout<<"player1 win\n";
+                    else  cout<<"bot win\n";
             }
-          else if(strcmp("rock",player1))
-                    {
-                        if(strcmp("paper",bot))
-                        {
-                            cout<<"bot win";
-                        }
-                        else
-                        {
-                            cout<<"player1 win";
-                        }
-                      }
+         else if(!strcmp("rock",player1))
+            {
+                    if(!strcmp("paper",bot))
+                    cout<<"bot win\n";
+                    else cout<<"player1 win\n";
             }
-
-
+        
+    }
     int main()
     {
         srand(time(0));
@@ -85,8 +63,6 @@ void player:: check()
         a.chub();
         a.bot1();
         a.check();
-        //a.check()
         return 0;
-
     }
 
